d6e4e21b98cf05172ebb2ac5bd2e1df745cf80a5
Byron <byron303@gmail.com>
2024-07-11 13:00:36 -0600

    deploying

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://virtus-bookstore-front.onrender.com",
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
